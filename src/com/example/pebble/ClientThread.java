fac93167d5d557ce9521f17e49297c986a9d11a1
itskvenki26 <itskvenki26@gmail.com>
2015-04-14 16:49:39 -0700

    comment

---
+++ src/com/example/pebble/ClientThread.java
@@ -0,0 +1,104 @@
+package com.example.pebble;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+public class ClientThread extends Thread {
+
+    public static final int TIMEOUT = 5000; // 5 sec timeout
+    public static final String KEY_MESSAGE = "message";
+
+    private Activity mActivity;
+
+    private Handler mHandler;
+    private Socket mSocket;
+    private InetSocketAddress mServerAddress;
+
+    private boolean mIsRunning = false;
+
+    public ClientThread(String serverIp, int serverPort, Handler handler, Activity activity) {
+        mActivity = activity;
+        mServerAddress = new InetSocketAddress(serverIp, serverPort);
+        mHandler = handler;
+        mSocket = new Socket();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        mIsRunning = true;
+
+        try {
+            if (!mSocket.isConnected()) {
+                mSocket.connect(mServerAddress);
+            }
+
+            InputStream socketStream = mSocket.getInputStream();
+
+            byte[] data = new byte[7];
+            while (mIsRunning) {
+                socketStream.read(data);
+
+                if (data[0] == 0x01 || data[0] == 0x02) {
+                    Message message = new Message();
+                    Bundle bundle = new Bundle();
+
+                    bundle.putByteArray(KEY_MESSAGE, data);
+                    message.setData(bundle);
+                    mHandler.sendMessage(message);
+                }
+            }
+
+            socketStream.close();
+            mSocket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketTimeoutException e) {
+            e.printStackTrace();
+        } catch (ConnectException e) {
+            showErrorToast(e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        if (mIsRunning) {
+            mIsRunning = false;
+        }
+
+        if (mSocket != null) {
+            try {
+                mSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showErrorToast(final ConnectException e) {
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mActivity, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public boolean isRunning() {
+        return mIsRunning;
+    }
+}
---
+++ src/com/example/pebble/ConnectDialog.java
@@ -0,0 +1,83 @@
+package com.example.pebble;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ConnectDialog extends DialogFragment {
+
+    public static final String TAG = "connect_dialog";
+
+    private EditText mServerIpEditText;
+    private EditText mServerPortEditText;
+
+    public ConnectDialog() {
+        // Empty constructor
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final MainActivity activity = (MainActivity) getActivity();
+
+        View view = activity.getLayoutInflater().inflate(R.layout.dialog_connect, null);
+        mServerIpEditText = (EditText) view.findViewById(R.id.edit_text_server_ip);
+        mServerPortEditText = (EditText) view.findViewById(R.id.edit_text_server_port);
+        mServerIpEditText.requestFocus();
+
+        String serverIp = getSavedServer();
+        if (!TextUtils.isEmpty(serverIp)) {
+            mServerIpEditText.setText(serverIp);
+            mServerIpEditText.selectAll();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setView(view);
+        builder.setTitle(R.string.title_dialog_connect);
+        builder.setCancelable(false);
+        builder.setPositiveButton(R.string.connect, null);
+
+        final AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String serverIp = mServerIpEditText.getText().toString();
+                        String serverPortString = mServerPortEditText.getText().toString();
+
+                        if (!TextUtils.isEmpty(serverIp) && !TextUtils.isEmpty(serverPortString)) {
+                            int serverPort = Integer.parseInt(serverPortString);
+                            activity.connect(serverIp, serverPort);
+                            dismiss();
+                        } else {
+                            Toast.makeText(activity, "Please enter valid server IP and port number", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return dialog;
+    }
+
+    private String getSavedServer() {
+        SharedPreferences prefs = getActivity().getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE);
+        String serverIp = prefs.getString(MainActivity.PREFS_KEY_SERVER_IP, "");
+        return serverIp;
+    }
+}
